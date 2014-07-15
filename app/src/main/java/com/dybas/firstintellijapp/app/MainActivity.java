e87cad4c43604532f54d748a16f879be05041de2
majster <majster@gmail.com>
2014-07-15 21:34:29 +0200

    team 1 does coding

--- app/src/main/java/com/dybas/firstintellijapp/app/MainActivity.java
+++ app/src/main/java/com/dybas/firstintellijapp/app/MainActivity.java
@@ -12,6 +12,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initClass();
+    }
+
+    private void initClass() {
+
     }
 
     @Override
