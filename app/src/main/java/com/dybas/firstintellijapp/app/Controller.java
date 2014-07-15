51bf0223b52ee3999637226b0ca63ea345550264
majster <majster@gmail.com>
2014-07-15 20:04:46 +0200

    some minor changes

--- app/src/main/java/com/dybas/firstintellijapp/app/Controller.java
+++ app/src/main/java/com/dybas/firstintellijapp/app/Controller.java
@@ -1,7 +1,9 @@
 package com.dybas.firstintellijapp.app;
 
-/**
- * Created by majster on 15.07.14.
- */
 public class Controller {
+    private final String name;
+
+    public Controller (String name ){
+        this.name = name;
+    }
 }
